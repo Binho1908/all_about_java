3a2a5d505252eda36484ecd3818f7fe8caca4b4c
Binho1908 <fabiosenninhaddt@gmail.com>
2021-10-02 20:06:06 -0300

    iniciado curso de java, modulo 1, 2, 3, completos e iniciando o modulo 4

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá mundo!"); // o println tem uma quebra de linha no final
+		System.out.println("Olá mundo!");
+		System.out.print("Olá mundo!"); // não tem quebra de linha no final
+		System.out.print("Olá mundo!");
+	}
+
+}
