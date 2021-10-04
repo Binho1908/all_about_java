15f55fad4865e633499062591445087f2716fe55
Binho1908 <fabiosenninhaddt@gmail.com>
2021-10-04 14:43:27 -0300

    termino do modulo 5

---
+++ src/estrutura_condicional/escopo_inicializacao.java
@@ -0,0 +1,21 @@
+package estrutura_condicional;
+
+import java.util.Scanner;
+
+public class escopo_inicializacao {
+	
+	public static void main(String[] args) {
+
+		double price = 400.00;
+		
+		double discont;
+		
+		if (price < 200.00) {
+			discont = price * 0.1;
+		}else {
+			discont = 0;
+		}
+		
+		System.out.println(discont);
+	}
+}
---
+++ src/estrutura_condicional/operacao_ternaria.java
@@ -0,0 +1,24 @@
+package estrutura_condicional;
+
+public class operacao_ternaria {
+
+	public static void main(String[] args) {
+		/*
+		 //SEM CONDIÇÃO TERNARIA
+		 double preco = 34.5;
+		 double desconto;
+		 if( preco < 20.0){
+		 	desconto = preco * 0.1;
+		 }else{
+		 	desconto = preco * 0.05;
+		 }
+		*/
+		
+		double preco = 34.5;
+		
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+		
+		System.out.println(desconto);
+	}
+
+}
